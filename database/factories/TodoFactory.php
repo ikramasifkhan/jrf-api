9137a0a25e91dc5464c2e2675899cf3a29cb4dad
ïkramasifkhanradi <iakriubat2016@gmail.com>
2022-11-03 00:46:53 +0600

    first commit

---
+++ database/factories/TodoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ */
+class TodoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence(5),
+            'status'=>$this->faker->randomElement(['checked', 'unchecked']),
+            //'category_id'=>Category::all()->random()->id,
+        ];
+    }
+}
